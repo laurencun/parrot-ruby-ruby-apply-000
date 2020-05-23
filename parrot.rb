7664c742d0a785b46e5e3b20e4fc7633e6e57e0b
Lauren Cunningham <lauren.busbee@gmail.com>
2020-05-23 16:51:53 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,8 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 def parrot(says = "Squawk!")
-  return puts "#{says}"
+  puts "#{says}"
+  return #{puts}
 end
 
 parrot
